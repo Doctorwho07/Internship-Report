662bd7dc6bca264cbe022e2f8bd93dc417a73e45
Docotrwho07 <alexis.rouches@icloud. com>
2024-08-15 23:16:21 +0200

    path optimization

---
+++ src/scripts/about.js
@@ -0,0 +1,12 @@
+import "/src/styles/style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+document.querySelector("#app").innerHTML = `
+  <div>
+    <h1>About Us</h1>
+    <p>Learn more about our team and what we do at Cimra!</p>
+    <button type="button" class="btn btn-secondary">Secondary Button</button>
+  </div>
+`;
