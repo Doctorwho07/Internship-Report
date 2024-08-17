c110b1eab476db00adda7a7b7ce814568d915619
Doctorwho07 <alexis.rouches@icloud.com>
2024-08-17 12:35:38 +0200

    back homepage

--- src/scripts/theme-toggle.js
+++ src/scripts/theme-toggle.js
@@ -1,18 +1,35 @@
 function toggleTheme() {
-  const theme = document.documentElement.getAttribute("data-theme");
-  if (theme === "dark") {
-    document.documentElement.setAttribute("data-theme", "light");
-    document
-      .getElementById("theme-stylesheet")
-      .setAttribute("href", "/src/styles/theme-light.css");
-  } else {
+  const themeToggle = document.getElementById("theme-toggle");
+  const themeLabel = document.getElementById("theme-label");
+
+  if (themeToggle.checked) {
     document.documentElement.setAttribute("data-theme", "dark");
     document
       .getElementById("theme-stylesheet")
       .setAttribute("href", "/src/styles/theme-dark.css");
+    themeLabel.textContent = "Dark";
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    document
+      .getElementById("theme-stylesheet")
+      .setAttribute("href", "/src/styles/theme-light.css");
+    themeLabel.textContent = "Light";
   }
 }
 
-document
-  .querySelector("#theme-toggle-btn")
-  .addEventListener("click", toggleTheme);
+// Charger le thème initial basé sur le `data-theme` ou par défaut
+document.addEventListener("DOMContentLoaded", () => {
+  const themeToggle = document.getElementById("theme-toggle");
+  const themeLabel = document.getElementById("theme-label");
+
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  if (currentTheme === "dark") {
+    themeToggle.checked = true;
+    themeLabel.textContent = "Dark";
+  } else {
+    themeToggle.checked = false;
+    themeLabel.textContent = "Light";
+  }
+});
+
+document.querySelector("#theme-toggle").addEventListener("change", toggleTheme);
