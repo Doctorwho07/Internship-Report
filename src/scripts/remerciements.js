22012db2fe249cd96e8f6caa6ecbc5988f4e06bf
Doctorwho07 <alexis.rouches@icloud.com>
2024-08-18 22:33:28 +0200

    feat remerciements content

--- src/scripts/remerciements.js
+++ src/scripts/remerciements.js
@@ -1,14 +1,29 @@
 export default function () {
   return `
-      <div class="container mt-5">
-        <h1>Remerciements</h1>
-        <p>Nous souhaitons remercier toutes les personnes qui ont contribué à notre projet.</p>
-        <ul>
-          <li>Merci à l'équipe de développement.</li>
-          <li>Merci à nos partenaires.</li>
-          <li>Merci à notre communauté.</li>
-        </ul>
-      </div>
-      <button type="button" id="homeLink" class="btn btn-primary">Retour à l'accueil</button>
-    `;
+        <div class="container mt-5 remerciements-container">
+          <h1 class="remerciements-title">Remerciements</h1>
+          <p class="remerciements-text">Je souhaite remercier toutes les personnes qui ont contribué au bon déroulement de mon stage.</p>
+          <ul class="remerciements-list">
+            <li class="remerciements-item">
+              Merci aux responsables 
+              <a href="https://www.linkedin.com/in/nicolasbert/" target="_blank" class="linkedin-link"><strong>Nicolas BERT</strong></a>
+              et 
+              <a href="https://www.linkedin.com/in/antoine-di-rienzo-155855151/" target="_blank" class="linkedin-link"><strong>Antoine DI RIENZO</strong></a>.
+            </li>
+            <li class="remerciements-item">
+              Merci aux alternants 
+              <a href="https://www.linkedin.com/in/lucas-sender-544131237/" target="_blank" class="linkedin-link"><strong>Lucas SENDER</strong></a>
+              et 
+              <strong>Anisse</strong>.
+            </li>
+            <li class="remerciements-item">
+              Merci aux différents clients de l'entreprise <strong>CIMRA</strong>.
+            </li>
+            <li class="remerciements-item">
+              Merci à l'équipe relation école et entreprise.
+            </li>
+          </ul>
+          <button type="button" id="homeLink" class="btn btn-primary custom-btn">Retour à l'accueil</button>
+        </div>
+      `;
 }
