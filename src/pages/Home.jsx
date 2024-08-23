78f947748dee74ad964f2719e356915fc687a589
Doctorwho07 <alexis.rouches@icloud.com>
2024-08-23 21:44:18 +0200

    img update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => (
     <div
       className="jumbotron text-center bg-light p-5 mb-5"
       style={{
-        backgroundImage: "url('/images/background.jpg')",
+        backgroundImage: "url('/logo/logo.png')",
         backgroundSize: "cover",
         backgroundPosition: "center",
         height: "400px",
